99fd42d4c27ddf10de063f4b81fcfb5193fb24d8
lej99 <eunji_ouo@naver.com>
2022-12-14 17:22:30 +0900

    22.12.14(0)

--- js/main.js
+++ js/main.js
@@ -19,12 +19,14 @@ $(function () {
     $('#popup>li:nth-of-type(5)').click(function () {
         $('.hidden:nth-of-type(5)').fadeIn();
         $('.hidden').not(':nth-of-type(5)').hide();
-        
+
     });
 
     $('.hidden').click(function () {
         $('.hidden').hide();
     });
+
+    if (window.matchMedia("(min-width: 960px)").matches) {
 
     var page = 0;
     $('.prev').click(function () {
@@ -33,7 +35,7 @@ $(function () {
             page = 0;
             return;
         }
-        $('.slide').stop().animate({ marginLeft: -350 * page }, 800);
+        $('.slide').stop().animate({ marginLeft: -450 * page }, 800);
     });
 
     $('.next').click(function () {
@@ -42,7 +44,30 @@ $(function () {
             page = $('.slide > div').length - 2;
             return;
         }
-        $('.slide').stop().animate({ marginLeft: -350 * page }, 800);
+        $('.slide').stop().animate({ marginLeft: -450 * page }, 800);
     });
+
+    } else {
+
+        var page = 0;
+        $('.prev').click(function () {
+            page--;
+            if (page < 0) {
+                page = 0;
+                return;
+            }
+            $('.slide').stop().animate({ marginLeft: -450 * page }, 800);
+        });
+    
+        $('.next').click(function () {
+            page++;
+            if (page > $('.slide > div').length - 1) {
+                page = $('.slide > div').length - 1;
+                return;
+            }
+            $('.slide').stop().animate({ marginLeft: -450 * page }, 800);
+        });
+
+    }
 
 });
